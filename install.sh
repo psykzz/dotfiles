d76b413c18e46f05b40b742f5d5a828d79a9b31e
Matt Smith <psykzz@users.noreply.github.com>
2017-03-09 15:38:05 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ brew install python python3 nvm git zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Get oh-my-zsh theme, aliases, and profile
-curl -fsSL https://raw.githubusercontent.com/psykzz/shell/master/oh-my-zsh/themes/psykzz.zsh-theme > ~/.oh-my-zsh/themes/psykzz.zsh-theme
-curl -fsSL https://raw.githubusercontent.com/psykzz/shell/master/dot.aliases > ~/.aliases
+curl -fsSL https://raw.githubusercontent.com/psykzz/dotfiles/master/oh-my-zsh/themes/psykzz.zsh-theme > ~/.oh-my-zsh/themes/psykzz.zsh-theme
+curl -fsSL https://raw.githubusercontent.com/psykzz/dotfiles/master/dot.aliases > ~/.aliases
 echo "export ZSH=$(echo ~)/.oh-my-zsh" > ~/.zshrc
-curl -fsSL https://raw.githubusercontent.com/psykzz/shell/master/dot.zshrc >> ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/psykzz/dotfiles/master/dot.zshrc >> ~/.zshrc
 
 # install and use the latest node
+export NVM_DIR="$(echo ~)/.nvm"
+source $(brew --prefix nvm)/nvm.sh
 nvm install --lts
 
 # Install java
