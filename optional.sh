9da6ec01bd02a6e01b0bfb6eba25c029edac89cf
Matt Smith <psykzz@users.noreply.github.com>
2017-05-05 09:54:17 +0100

    Update optional.sh

--- optional.sh
+++ optional.sh
@@ -1,1 +1,15 @@
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+confirm "Would you like to install optional components? [y/N] " || exit 0;
 echo "Nothing to see here"
